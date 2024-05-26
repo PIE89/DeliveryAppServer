cd0521ba7669cdbf4d0596303587c21c44542eb1
PIE89 <spyder34@yandex.ru>
2024-05-26 17:58:22 +0300

    added data for server

---
+++ index.js
@@ -0,0 +1,16 @@
+// server.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use(function (req, res, next) {
+  setTimeout(next, 800);
+});
+
+server.use(router);
+server.listen(8000, () => {
+  console.log("Server ready");
+});
